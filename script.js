389f3d1dedfeb1e655009cc6cbd7556783ce1e18
swaroop <swaroop.uprety@kroger.com>
2019-06-02 13:53:51 -0400

    working with conditional statements

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ var name = "Swaroop";
 document.body.innerHTML = "<h1>Today is: " + date + "</h1>";
 
 
-var a = 4;
+var a = 5;
 var b = 5;
-var sum = a+b;
+var theNumbersMatch;
+
+if (a == b) {
+  theNumbersMatch = true;
+}else {
+  theNumbersMatch = false;
+}
+
+console.log("The numbers match: " + theNumbersMatch);
