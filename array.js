c32cb1e0be904e525711ff6045c2754a78c99f39
swaroop <swaroop.uprety@kroger.com>
2019-06-02 14:32:25 -0400

    playing around with array methods

--- array.js
+++ array.js
@@ -4,4 +4,16 @@ pens = ["red", "blue", "green", "orange"];
 
 var fourthPen = pens[3];
 
-console.log(pens[2]);
+console.log("Before: ", pens);
+
+console.log("Array Length: " + pens.length);
+
+// pens.reverse();
+
+var result = pens.indexOf("orange", 1);
+console.log("The value is: " + result);
+
+
+//joins
+var arrayString = pens.join(" - ");
+console.log("String from array: " + arrayString);
