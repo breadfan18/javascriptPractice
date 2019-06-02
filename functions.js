49b415cecb28625510b2f19aeb349798c3eca5f2
swaroop <swaroop.uprety@kroger.com>
2019-06-02 16:54:25 -0400

    added some code to function.js

--- functions.js
+++ functions.js
@@ -16,3 +16,22 @@ var a = 3/4;
 var b = 5/7;
 
 findBiggestFraction();
+
+prompt("We need some information", "Enter your name");
+
+var result = confirm("Do you really want to leave?");
+if (result) {
+    alert("Thanks for visiting");
+}
+else {
+  alert("Thank you for staying")
+}
+
+function test (number){
+  while (number < 5){
+    number++;
+  }
+  return number;
+}
+
+alert(test(2));
