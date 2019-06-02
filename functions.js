5701638b9ec17d526da1b8adcac87eecf8c9a662
swaroop <swaroop.uprety@kroger.com>
2019-06-02 14:43:13 -0400

    added a new js file called functions

---
+++ functions.js
@@ -0,0 +1,18 @@
+function multiply (a, b){
+  var result = a*b;
+  console.log(result);
+  return result;
+}
+
+console.log(multiply(3,4));
+
+
+function findBiggestFraction(){
+  a > b ? console.log("a: ", a) : console.log("b: ", b);
+}
+
+
+var a = 3/4;
+var b = 5/7;
+
+findBiggestFraction();
