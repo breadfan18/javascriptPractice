c3d3aca98d091e84046e730a95fa38454308556b
swaroop <swaroop.uprety@kroger.com>
2019-06-05 13:48:04 -0400

    wrapped up practice for normal functions

--- functions.js
+++ functions.js
@@ -12,3 +12,7 @@ var secondFraction = 5/7;
 var fractionResult  = findBiggestFraction(firstFraction, secondFraction);
 
 console.log(fractionResult);
+
+console.log("First fraction: ", firstFraction);
+console.log("Second fraction: ", secondFraction);
+console.log("Fraction " + fractionResult[0] + " with a value of " + fractionResult[1] + " is bigger");
