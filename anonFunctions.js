f86d7381b1ce9c3eb6cd5d1731ee00f061c5942e
swaroop <swaroop.uprety@kroger.com>
2019-06-05 14:57:10 -0400

    done with anon functions, working on variable scope now

--- anonFunctions.js
+++ anonFunctions.js
@@ -1,6 +1,6 @@
 
 //to use anonymous functions, you create a variable and then assign it with just an unnamed function(), followed by a code block
-var theBiggest = function (a, b) {
+var theBiggest1 = function (a, b) {
     var result;
     a > b ? result = ["a: ", a] : result = ["b: ", b];
     //console.log(result);
@@ -9,16 +9,21 @@ var theBiggest = function (a, b) {
 
 //to use the function, you call the variable name with parenthesis as below:
 //if the anonymous function takes arguments, then when the variable is called, then you add the arguments in the parenthesis.
-console.log(theBiggest(7/9, 13/25));
+console.log(theBiggest1(7/9, 13/25));
 
 //immediately invoked function expression
 // first, wrap the entire anon function a () and then add a new () at the end of the function code block, meaning after the }, and pass any arguments inside that parenthesis.
 //in this example above, immediately invoked function would like like below:
-var theBiggest = (function (a, b) {
+var firstFraction = 7/9;
+var secondFraction = 13/25;
+
+var theBiggest2 = (function (a, b) {
     var result;
     a > b ? result = ["a: ", a] : result = ["b: ", b];
     //console.log(result);
     return result;
-})(7/9, 13/25)
+})(firstFraction, secondFraction)
 //now, as a result, you can console log out just the variable, meaning without the () and it will work the same way
-console.log(theBiggest);
+console.log(theBiggest2);
+
+
