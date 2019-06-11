b8bc791935abd687685c4a3b846a14b0613f2c90
breadfan18 <swaroop.uprety@kroger.com>
2019-06-11 17:13:27 -0400

    added code to manipulate elements

--- 05_06/JS/script.js
+++ 05_06/JS/script.js
@@ -1,1 +1,16 @@
+const FEATURED = document.querySelector(".featured-image");
+const THEIMAGE = document.querySelector("img");
 
+var altText = THEIMAGE.getAttribute("alt");
+
+var captionElement = document.createElement("figCaption");
+
+var captionText = document.createTextNode(altText);
+
+captionElement.appendChild(captionText);
+
+console.log(captionElement);
+
+FEATURED.appendChild(captionElement);
+
+THEIMAGE.setAttribute("alt", "");
