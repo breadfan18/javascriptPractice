43a7bf6eef1c4f1a3defb18313ea564c181dcafd
breadfan18 <swaroop.uprety@kroger.com>
2019-06-11 16:02:54 -0400

    minor changes in html file

--- closures.js
+++ closures.js
@@ -4,11 +4,15 @@ function doSomeMath(){
     var a = 5;
     var b = 4;
 
-    
+    function multiply(){
+        var result = a * b;
+        return result;
+    }
 
-    return sum;
+
+    return multiply;
 }
 
 var theResult = doSomeMath();
 
-console.log("The result: ", theResult);
+console.log("The result: ", theResult());
