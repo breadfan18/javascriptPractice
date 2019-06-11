8feb38b7b19e724dc45d0c9b2868376bd6b3b48f
breadfan18 <swaroop.uprety@kroger.com>
2019-06-11 15:55:43 -0400

    added a new js for closure examples

---
+++ closures.js
@@ -0,0 +1,14 @@
+//closures are basically a function running inside of another function
+
+function doSomeMath(){
+    var a = 5;
+    var b = 4;
+
+    
+
+    return sum;
+}
+
+var theResult = doSomeMath();
+
+console.log("The result: ", theResult);
