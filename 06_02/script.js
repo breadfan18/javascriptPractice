99b53d5a014c467646e4531f859a5d2375e2bc60
breadfan18 <swaroop.uprety@kroger.com>
2019-06-12 12:55:09 -0400

    minor changes

--- 06_02/script.js
+++ 06_02/script.js
@@ -7,5 +7,7 @@ let minPosition = 130;
 let secPosition = 267;
 
 HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-MINUTEHAND.style.transform = "rotate("")"
+MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+
 
