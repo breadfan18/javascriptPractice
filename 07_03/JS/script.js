90610dd6ad06e2c3646ce743c3f9e7f0c8956934
breadfan18 <swaroop.uprety@kroger.com>
2019-06-12 14:40:15 -0400

    more code

--- 07_03/JS/script.js
+++ 07_03/JS/script.js
@@ -1,1 +1,15 @@
+const CTA = document.querySelector(".cta a");
+const ALERT = document.querySelector("#booking-alert");
 
+CTA.classList.remove("hide");
+ALERT.classList.add("hide");
+
+function reveal(e) {
+    //e is short for event. prevents the default event. for eg, the CTA variable, in html has an href that it tries to open by default
+    //this preventDefault method will stop that from happening, specifically when the reveal function is called.
+    e.preventDefault();
+    CTA.classList.toggle("hide");
+    ALERT.classList.toggle("hide");
+}
+
+CTA.onclick = reveal;
