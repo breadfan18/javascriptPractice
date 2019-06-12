0662524b119dd547bb6d8a6420db4479e5a0de72
breadfan18 <swaroop.uprety@kroger.com>
2019-06-12 13:15:55 -0400

    added ex files 06-05 to project

--- 06_05/script.js
+++ 06_05/script.js
@@ -8,16 +8,11 @@ let secPosition = 267;
 
 var date = new Date();
 console.log(date);
-
 let hr = date.getHours();
 let min = date.getMinutes();
 let sec = date.getSeconds();
-
-console.log("Hour: " + hr + " Min: " + min + " Sec: " +sec);
-
+console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
 
 HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
 MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
 SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
-
-
