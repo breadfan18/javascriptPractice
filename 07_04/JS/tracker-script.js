d30a5f624b8a5cfc05d072fd0ee009732654553a
breadfan18 <swaroop.uprety@kroger.com>
2019-06-12 15:18:23 -0400

    trying to add a second circle in mouse-tracker.html

---
+++ 07_04/JS/tracker-script.js
@@ -0,0 +1,48 @@
+const AREA = document.body;
+const CIRCLE = document.querySelector('.circle');
+
+const CIRCLE2 = document.querySelector('.circle2');
+
+var windowWidth = window.innerWidth;
+var windowHeight = window.innerHeight;
+
+function mouseCoordinates(e) {
+    // Capture the event object (mouse movement).
+    // Get X coordinate (distance from left viewport edge) via clientX property.
+    // Take total window width, subtract current coordinate and width of circle.
+    // Do the same for Y coordinate (distance from top viewport edge).
+    var horizontalPosition = windowWidth - e.clientX - 26;
+    var verticalPosition= windowHeight - e.clientY - 26;
+
+    var horizontalPositionForCircle2 = windowWidth + e.clientX - 55;
+    var verticalPositionForCircle2 = windowHeight + e.clientX - 55;
+
+    // Set horizontal and vertical position.
+    CIRCLE.style.left = horizontalPosition + 'px';
+    CIRCLE.style.top = verticalPosition + 'px';
+
+    CIRCLE2.style.left = horizontalPositionForCircle2 + 'px';
+    CIRCLE2.style.top = verticalPositionForCircle2 + 'px';
+
+
+}
+
+function changeColorOnTouch() {
+    CIRCLE.style.backgroundColor = "green";
+    CIRCLE.style.borderColor = "green";
+
+    CIRCLE2.style.backgroundColor = "black";
+    CIRCLE2.style.borderColor = "black";
+}
+
+// When the mouse moves, run the mouseCoordinates function.
+AREA.addEventListener('mousemove', mouseCoordinates, false);
+
+// When the mouse touches the circle, run the changeColorOnTouch function.
+CIRCLE.addEventListener('mouseenter', changeColorOnTouch, false);
+CIRCLE2.addEventListener('mouseenter', changeColorOnTouch, false);
+
+
+// When the mouse leaves the circle, remove inline styles with an anonymous function.
+CIRCLE.addEventListener('mouseleave', function(){CIRCLE.removeAttribute("style");}, false);
+CIRCLE2.addEventListener('mouseleave', function(){CIRCLE2.removeAttribute("style");}, false);
