babc11f2db5d096adb3244f4ae1f24eae5a0a9e2
breadfan18 <swaroop.uprety@kroger.com>
2019-06-17 16:38:44 -0400

    added some comments to understand the continue keyword

--- 09_03/JS/primeScript.js
+++ 09_03/JS/primeScript.js
@@ -12,5 +12,13 @@ function primeTest(testValue) {
 
 for (let i = 2; i<=CEILING; i++) {
     let result = primeTest(i);
-    console.log(i + " is a prime number.");
+    //we use continue in order to only display the prime numbers: Continue means that if the result is false,
+    //if will ignore the rest of the code in the loop, and go back to the next iteration of the loop.
+    //that means if result returns false, that means the number is not a prime number, then it doesnt
+    //console out anything, and runs the next iteration..so in this way it will only console out
+    //for the numbers that return result = true, meaning prime numbers.
+   if (result == false) {
+       continue;
+   }
+    console.log(i + " is a prime number." + result);
 }
