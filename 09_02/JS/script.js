7edceaffa4a09c30899721f033789d1b7e37a64a
breadfan18 <swaroop.uprety@kroger.com>
2019-06-17 16:26:31 -0400

    added js code to add _blank target attribute to all http links

--- 09_02/JS/script.js
+++ 09_02/JS/script.js
@@ -2,5 +2,9 @@ var extLinks = document.querySelectorAll('a[href^="http"]');
 console.log(extLinks);
 
 for (let i = 0; i < extLinks.length; i++) {
-    console.log(extLinks[i]);
+    //console.log(extLinks[i]);
+    if (!extLinks[i].hasAttribute("target")) {
+        extLinks[i].setAttribute("target", "_blank");
+    }
+
 }
