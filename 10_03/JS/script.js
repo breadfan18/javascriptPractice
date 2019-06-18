4b5b7145af75133ea592a8253d2dd9c6f2c4904d
breadfan18 <swaroop.uprety@kroger.com>
2019-06-18 13:59:29 -0400

    some js code for responsive image sizing

--- 10_03/JS/script.js
+++ 10_03/JS/script.js
@@ -1,13 +1,26 @@
 const IMAGES = document.querySelectorAll("img");
 console.log(IMAGES);
 
+function makeSrcSet(imgSrc){
+    let markup = [];
+    let width = 400;
+
+    for (let i = 0; i < 5; i++) {
+        markup[i] = imgSrc + "-" + width + ".jpg " + width + "w";
+        width+=400;
+    }
+    //below will return a comma separated list with all values of the markup array
+    return markup.join();
+}
+
 for (let i = 0; i < IMAGES.length; i++) {
 
     let imgSrc = IMAGES[i].getAttribute("src");
 
     //stripping off the - 800.jpg (which is 8 characters) off of each image name
     imgSrc = imgSrc.slice(0, -8);
-    console.log(imgSrc);
+    let srcSet = makeSrcSet(imgSrc);
+    console.log(srcSet);
 
     let type = IMAGES[i].getAttribute("data-type");
     console.log(type);
