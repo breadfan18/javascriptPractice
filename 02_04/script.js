c28faea44e5931122987d5620ded2427a325e42c
Swaroop Uprety <breadfan18@gmail.com>
2020-10-18 16:45:32 -0400

    oct 17 commit 1

---
+++ 02_04/script.js
@@ -0,0 +1,11 @@
+
+var date = new Date();
+document.body.innerHTML = "<h1> Today's date is: " + date;
+
+var pens;
+
+pens = ["red", "green", "blue", "orangeX"];
+
+console.log(pens);
+
+console.log(pens[1]);
