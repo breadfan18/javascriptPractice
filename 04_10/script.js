6dae62c2df13eb29f645b03107bd3cbf1ccb507f
Swaroop <breadfan18@gmail.com>
2020-12-07 14:00:09 -0500

    dec 7 commit 3

--- 04_10/script.js
+++ 04_10/script.js
@@ -4,4 +4,30 @@ function Course(title, instructor, level, published, views) {
     this.level = level;
     this.published = published;
     this.views = views;
+    this.updateViews = function () {
+        return ++this.views;
+    };
 }
+
+var courses = [
+    new Course("JavaScript Essentiallll",
+    "Morten Rank Hendriksen",
+    1,
+    true,
+    0
+    ),
+
+    new Course("EcmaScript Basics",
+    "Swaroop Uprety",
+    3,
+    false,
+    20
+    )
+
+];
+
+
+
+
+console.log(course01);
+console.log(course02);
