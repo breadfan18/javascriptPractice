5b280b532590e82664333d9360bc8eff2a482960
Swaroop <breadfan18@gmail.com>
2020-12-09 17:50:33 -0500

    dec 9 commit 2

--- 05_06/JS/script.js
+++ 05_06/JS/script.js
@@ -3,20 +3,33 @@ const THEIMAGE = document.querySelector("img");
 
 var altText = THEIMAGE.getAttribute("alt");
 
-var captionElement = document.createElement("figCaption");
+var captionElement = document.createElement("figcaption");
 
 var captionText = document.createTextNode(altText);
 
 captionElement.appendChild(captionText);
-
-console.log(captionElement);
 
 FEATURED.appendChild(captionElement);
 
 THEIMAGE.setAttribute("alt", "");
 
 
-//theres a new append method that makes this procdess much easier. It can be done this way:
-captionElement.append(altText);
-FEATURED.append(captionElement);
-THEIMAGE.setAttribute("alt", "");
+// var altText = THEIMAGE.getAttribute("alt");
+//
+// var captionElement = document.createElement("figCaption");
+//
+// var captionText = document.createTextNode(altText);
+//
+// captionElement.appendChild(captionText);
+//
+// console.log(captionElement);
+//
+// FEATURED.appendChild(captionElement);
+//
+// THEIMAGE.setAttribute("alt", "");
+//
+//
+// //theres a new append method that makes this procdess much easier. It can be done this way:
+// captionElement.append(altText);
+// FEATURED.append(captionElement);
+// THEIMAGE.setAttribute("alt", "");
