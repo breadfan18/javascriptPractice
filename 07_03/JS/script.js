ee1f6ef590d0337aee42e0ea00624fb8e53bc831
Swaroop <breadfan18@gmail.com>
2021-01-20 18:27:31 -0500

    moonwalk manor

--- 07_03/JS/script.js
+++ 07_03/JS/script.js
@@ -6,15 +6,17 @@ const ALERT = document.querySelector("#booking-alert");
 CTA.classList.remove("hide");
 ALERT.classList.add("hide");
 
-function reveal(e) {
+function reveal(e, current) {
     //e is short for event. prevents the default event. for eg, the CTA variable, in html has an href that it tries to open by default
     //this preventDefault method will stop that from happening, specifically when the reveal function is called.
     e.preventDefault();
-    CTA.classList.toggle("hide");
+    current.innerHTML == "Book Now!" ? CTA.innerHTML = "Oooops!" : CTA.innerHTML = "Book Now!";
     ALERT.classList.toggle("hide");
 }
 
-CTA.addEventListener("click", reveal, false);
+CTA.addEventListener("click", function (e) {
+    reveal(e, this);
+}, false);
 CTA.addEventListener("click", function () {
     console.log("The button was clicked", false);
 });
